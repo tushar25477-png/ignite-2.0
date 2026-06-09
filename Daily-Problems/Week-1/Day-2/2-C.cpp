c4e8190f750d7237436862e6b37debc38ae4ce48
JeffTunner <abhinavkaushik05@gmail.com>
2026-06-09 09:59:02 +0530

    added 2-C

---
+++ Daily-Problems/Week-1/Day-2/2-C.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    string target = "hello";
+    int j = 0;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] == target[j]) {
+            j++;
+        }
+
+        if (j == target.length()) {
+            break;
+        }
+    }
+    if (j == target.length())
+        cout << "YES";
+    else
+        cout << "NO";
+
+    return 0;
+}
