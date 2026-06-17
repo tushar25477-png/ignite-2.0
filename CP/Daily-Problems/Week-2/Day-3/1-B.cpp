6b4ea53f5d454d730f5a60dd5c6f7775c8c662fe
JeffTunner <abhinavkaushik05@gmail.com>
2026-06-17 13:39:59 +0530

    added 1-B

---
+++ CP/Daily-Problems/Week-2/Day-3/1-B.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    string s;
+    getline(cin, s);
+
+    set<char> st;
+
+    for(char c : s) {
+        if(c >= 'a' && c <= 'z') {
+            st.insert(c);
+        }
+    }
+
+    cout << st.size();
+
+    return 0;
+}
