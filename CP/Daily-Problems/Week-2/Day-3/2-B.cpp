95ba43b5a2a4477f89b776a1b18995f32fa92e15
JeffTunner <abhinavkaushik05@gmail.com>
2026-06-17 13:40:17 +0530

    added 2-B

---
+++ CP/Daily-Problems/Week-2/Day-3/2-B.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+        for(int num : nums) {
+            freq[num]++;
+        }
+        vector<pair<int, int>> v;
+        for(auto it : freq) {
+            v.push_back({it.second, it.first});
+        }
+        sort(v.rbegin(), v.rend());
+        vector<int> ans;
+        for(int i = 0; i < k; i++) {
+            ans.push_back(v[i].second);
+        }
+        return ans;
+    }
+};
