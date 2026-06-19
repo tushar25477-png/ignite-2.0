0536917d162433569b37ad96c78afbb2062df279
diya-ag <diyaag1503@gmail.com>
2026-06-19 17:55:04 +0530

    added 2-A

---
+++ CP/Daily-Problems/Week-2/Day-5/2-A.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        
+        auto low = lower_bound(nums.begin(), nums.end(), target);
+        
+        if (low == nums.end() || *low != target) {
+            return {-1, -1};
+        }
+        
+        auto high = upper_bound(nums.begin(), nums.end(), target);
+        
+        int start = low - nums.begin();
+        int end = high - nums.begin() - 1;
+        
+        return {start, end};
+    }
+};
