9ccd6eb916291c2c95165010b405510a8247a916
ankit <rathaurankit501@gmail.com>
2026-06-23 13:29:22 +0530

    added 2-B.cpp

---
+++ CP/Daily-Problems/Week-3/Day-2/2-B.cpp
@@ -0,0 +1,16 @@
+class Solution {
+    bool solve(vector<int>& arr, int i) {
+        if (i == arr.size() - 1)
+            return true;
+
+        if (arr[i] > arr[i + 1])
+            return false;
+
+        return solve(arr, i + 1);
+    }
+
+public:
+    bool isSorted(vector<int>& arr) {
+        return solve(arr, 0);
+    }
+};
