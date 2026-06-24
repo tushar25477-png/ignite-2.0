c5cb7f71f5b333afa934a2c833b05e7aed415daf
ankit <rathaurankit501@gmail.com>
2026-06-24 19:25:47 +0530

    added 2-A.cpp

--- CP/Daily-Problems/Week-3/Day-3/2-A.cpp
+++ CP/Daily-Problems/Week-3/Day-3/2-A.cpp
@@ -1,9 +1,28 @@
 class Solution {
-public:
-  int gcd(int a, int b) {
-    if (b == 0)
-      return a;
+  void solve(vector<int> &arr, int idx, vector<int> &curr,
+             vector<vector<int>> &ans) {
 
-    return gcd(b, a % b);
+    if (idx == arr.size()) {
+      ans.push_back(curr);
+      return;
+    }
+
+    // Pick
+    curr.push_back(arr[idx]);
+    solve(arr, idx + 1, curr, ans);
+    curr.pop_back();
+
+    // Not Pick
+    solve(arr, idx + 1, curr, ans);
+  }
+
+public:
+  vector<vector<int>> subsets(vector<int> &arr) {
+    vector<vector<int>> ans;
+    vector<int> curr;
+
+    solve(arr, 0, curr, ans);
+
+    return ans;
   }
 };
