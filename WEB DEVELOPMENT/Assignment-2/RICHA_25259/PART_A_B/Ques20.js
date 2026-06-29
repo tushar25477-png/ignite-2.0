af0701f2918a5ae47219f83d98089801bbb512a4
Sumit Awasthi <sumit.24660@knit.ac.in>
2026-06-29 12:15:59 +0530

    Update Ques20.js

--- WEB DEVELOPMENT/Assignment-2/RICHA_25259/PART_A_B/Ques20.js
+++ WEB DEVELOPMENT/Assignment-2/RICHA_25259/PART_A_B/Ques20.js
@@ -9,3 +9,20 @@ function findSecondLargest(arr){
     return max2;
 }
 console.log(findSecondLargest([22,4 ,55 ,55,89]));
+//huge blunder in your solution, you are not updating max2 correctly
+//here is the correct one
+function findSecondLargest(arr) {
+    let max1 = -Infinity;
+    let max2 = -Infinity;
+
+    for (let num of arr) {
+        if (num > max1) {
+            max2 = max1;
+            max1 = num;
+        } else if (num > max2 && num !== max1) {
+            max2 = num;
+        }
+    }
+
+    return max2 === -Infinity ? null : max2;
+}
